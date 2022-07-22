e1d1136905c86bf6502de732559a162999395e94
Payal Jindal (MountBlue Technologies Private) <v-pajindal@microsoft.com>
2022-07-22 13:03:49 +0530

    testclient added

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,14 @@
 from app.database_definitions import get_mental_health_champions
+from app.main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+
+def test_read_main() -> None:
+    """Testing root path"""
+    response = client.get("/")
+    assert response.status_code == 200
 
 
 def test_get_mental_health_champions(db_session, populate_db):
