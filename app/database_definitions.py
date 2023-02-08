513c65a8543d9e81d5697fae647cebcfaf672b72
Payal Jindal (MountBlue Technologies Private) <v-pajindal@microsoft.com>
2023-02-08 12:51:20 +0530

    added mutations and queries

--- app/database_definitions.py
+++ app/database_definitions.py
@@ -12,3 +12,42 @@ def get_mental_health_champions(session: Session):
         result = session.execute(query).scalars().unique().all()
 
     return result
+
+
+def get_champion_by_id(id: int, session: Session):
+    """Return champion data for given id"""
+    query = select(ChampionsModel).filter(ChampionsModel.id == id)
+    with session as session:
+        result = session.execute(query).scalars().unique().one()
+    return result
+
+
+def update_champion(id, champion_data, session: Session):
+    """Return updated champion data"""
+    query = select(ChampionsModel).filter(ChampionsModel.id == id)
+    with session as session:
+        champion = session.execute(query).scalars().unique().one()
+        if champion_data["name"]:
+            champion.name = champion_data["name"]
+        if champion_data["biography"]:
+            champion.biography = champion_data["biography"]
+        if champion_data["linkedin"]:
+            champion.linkedin = champion_data["linkedin"]
+        if champion_data["msr_profile"]:
+            champion.msr_profile = champion_data["msr_profile"]
+        if champion_data["avatar"]:
+            champion.avatar = champion_data["avatar"]
+        if champion_data["order"]:
+            champion.order = champion_data["order"]
+
+
+def create_champion(champion_data, session: Session):
+    """Create a champion profile"""
+    schema = ChampionsModel(
+        name=champion_data["name"],
+        biography=champion_data["biography"],
+        linkedin=champion_data["linkedin"],
+        msr_profile=champion_data["msr_profile"],
+        avatar=champion_data["avatar"],
+        order=champion_data["order"],
+    )
--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
 from app import models
-from ariadne import QueryType, gql, load_schema_from_path, make_executable_schema
+from ariadne import MutationType, QueryType, gql, load_schema_from_path, make_executable_schema
 from ariadne.asgi import GraphQL
 from ariadne.contrib.federation.objects import FederatedObjectType
 from ariadne.contrib.federation.schema import make_federated_schema
 from fastapi import FastAPI
 
-from .database_definitions import get_mental_health_champions
+from .database_definitions import (
+    create_champion,
+    get_champion_by_id,
+    get_mental_health_champions,
+    update_champion,
+)
 from .db import SessionLocal, engine
 
 app = FastAPI()
@@ -16,20 +21,51 @@ type_defs = gql(type_defs)
 
 # Create type instance for Query type defined in our schema...
 query = QueryType()
+mutation = MutationType()
 champions = FederatedObjectType("Champions")
 
 # ------------ QUERIES START HERE ------------------#
+
 
 @champions.reference_resolver
 def resolve_champions_data(_, info, representation):
     session = info.context["session"]
     return get_mental_health_champions(session)
 
+
 @query.field("getMentalHealthChampions")
-def resolve_get_welcome_screens(_, info):
+def resolve_get_champions_data(_, info):
     """Return sqlalchemy object for schema provided"""
     session = info.context["session"]
     return get_mental_health_champions(session)
+
+
+@query.field("champions")
+def resolve_champions(_, info):
+    """Return all the champions from database"""
+    session = info.context["session"]
+    return get_mental_health_champions(session=session)
+
+
+@query.field("champion")
+def resolve_champion(_, info, id):
+    """Return champion data by id"""
+    session = info.context["session"]
+    return get_champion_by_id(id=id, session=session)
+
+
+@mutation.field("addChampion")
+def resolve_add_champion(_, info, champion_data):
+    """Create a ne champion from champion data"""
+    session = info.context["session"]
+    return create_champion(champion_data=champion_data, session=session)
+
+
+@mutation.field("updateChampion")
+def resolve_update_champion(_, info, id, champion_data):
+    """Create a ne champion from champion data"""
+    session = info.context["session"]
+    return update_champion(id=id, champion_data=champion_data, session=session)
 
 
 def get_context_value(request):
@@ -39,5 +75,5 @@ def get_context_value(request):
 # -------- QUERIES END HERE ------------#
 
 
-schema = make_federated_schema(type_defs, [query])  # type: ignore
+schema = make_federated_schema(type_defs, [query, mutation])  # type: ignore
 app.mount("/", GraphQL(schema, context_value=get_context_value))
